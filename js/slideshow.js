30104e4a86983820889468da991838df8d40b16d
jacobota <otajacob@yahoo.com>
2023-06-13 00:09:05 -0700

    Fixed Facebook link, added more photos to slides

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,7 +3,17 @@ const slides = [
     '../images/Pic2.jpg',
     '../images/Pic4.jpg',
     '../images/Pic5.jpg',
-    '../images/Pic6.jpg'
+    '../images/Pic6.jpg',
+    '../images/Pic7.jpeg',
+    '../images/Pic8.jpeg',
+    '../images/Pic9.jpg',
+    '../images/Pic10.jpeg',
+    '../images/Pic11.jpg',
+    '../images/Pic12.jpg',
+    '../images/Pic13.jpg',
+    '../images/Pic14.jpeg',
+    '../images/Pic15.jpeg',
+    '../images/Pic16.jpeg'
 ]
 
 let slideholder = document.getElementById("slideshowimage");
